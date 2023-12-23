47554e7d0e809b6f1711109b073983c6bc198e45
luke andy <2949366226@qq.com>
2023-12-24 01:52:29 +0800

    filelist文件提交

---
+++ filelist/IMG2COL_GEMM.f
@@ -0,0 +1,16 @@
+../rtl/IMG2COL_GEMM.v
+../tb/tb_IMG2COL_GEMM.sv
+../rtl/img2col.v
+../rtl/matrix_mul.v
+../rtl/ram_t.sv
+../rtl/ram_w.sv
+../rtl/ram_save.v
+../rtl/data_process.v
+../rtl/tensor_addr.v
+../rtl/weight_addr.v
+../rtl/mul_add.v
+../rtl/matrix_add.v
+../rtl/result_process.v
+
+
+
